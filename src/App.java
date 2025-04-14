ae9c591d0a5bcaa40b4fb053e62939e6f46f7a5a
lmenne1 <lmenne1@smail.uni-koeln.de>
2025-04-14 13:26:39 +0200

    Präsenzaufgaben 1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,30 @@
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numberToGuess = (int)(Math.random() * 10) + 1;
+        Scanner scanner = new Scanner(System.in);
+        //variable that manages the games state
+
+        int i = 0;
+        while(i < 6 && !scanner.equals(numberToGuess)){
+            int z = scanner.nextInt();
+            if(z != numberToGuess){
+                System.out.println("lose!");
+                if(z < numberToGuess){
+                    System.out.println("Too low");
+                }
+                else{
+                    System.out.println("Too high");
+                }
+            }
+            else{
+                System.out.println("win!");
+                break;
+            }
+            i++;
+        }
+        
     }
 }
